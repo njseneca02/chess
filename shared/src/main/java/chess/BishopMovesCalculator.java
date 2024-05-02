61652aa50f041997320e2acb3c519e86e8c778a3
njseneca02 <njseneca@byu.edu>
2024-05-02 16:47:35 -0600

    created the PieceMovesCalculator interface and have started work to create a BishopMovesCalculator implemented by the interface

---
+++ shared/src/main/java/chess/BishopMovesCalculator.java
@@ -0,0 +1,9 @@
+package chess;
+
+import java.util.Collection;
+
+public class BishopMovesCalculator implements PieceMovesCalculator{
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
+        return null;
+    }
+}
