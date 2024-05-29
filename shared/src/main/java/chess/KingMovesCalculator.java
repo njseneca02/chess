76881d10bf803f0af15938989a044148969d4daf
njseneca02 <njseneca@byu.edu>
2024-05-28 22:09:58 -0600

    cleaned up all of the code

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -46,14 +46,4 @@ public class KingMovesCalculator implements PieceMovesCalculator{
         return result;
     }
 
-    public void addMoveHelper(ArrayList<ChessMove> result, int x, int y, ChessPosition myPosition, ChessBoard board, ChessGame.TeamColor myTeamColor){
-        if(myPosition.chgPosition(x,y).inBounds()){
-            ChessPosition moveTo = myPosition.chgPosition(x,y);
-            ChessPiece targetDest = board.getPiece(moveTo);
-            if(targetDest == null || targetDest.getTeamColor() != myTeamColor){
-                ChessMove add = new ChessMove(myPosition, moveTo, null);
-                result.add(add);
-            }
-        }
-    }
 }
--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,7 +1,19 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public interface PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition);
+
+    public default void addMoveHelper(ArrayList<ChessMove> result, int x, int y, ChessPosition myPosition, ChessBoard board, ChessGame.TeamColor myTeamColor){
+        if(myPosition.chgPosition(x,y).inBounds()){
+            ChessPosition moveTo = myPosition.chgPosition(x,y);
+            ChessPiece targetDest = board.getPiece(moveTo);
+            if(targetDest == null || targetDest.getTeamColor() != myTeamColor){
+                ChessMove add = new ChessMove(myPosition, moveTo, null);
+                result.add(add);
+            }
+        }
+    }
 }
