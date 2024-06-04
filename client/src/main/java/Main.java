b49821a35508eb9447c106b54807833694056674
njseneca02 <njseneca@byu.edu>
2024-06-04 09:49:00 -0600

    created classes to organize code and imported the tictactoe code to tweak into working as a chess board

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -2,6 +2,8 @@ import chess.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        //create menu
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
     }
