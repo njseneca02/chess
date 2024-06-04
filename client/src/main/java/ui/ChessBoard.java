935485d2f5e77687063dcdbac99b3a510c11660c
njseneca02 <njseneca@byu.edu>
2024-06-04 12:23:40 -0600

    both boards draw correctly, now I need to figure out writing pieces to them

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -12,29 +12,45 @@ public class ChessBoard {
     private static final String EMPTY = "   ";
     private static final String X = " X ";
     private static final String O = " O ";
+    private static final String[] whiteHeaders = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
+    private static final String[] blackHeaders = {" h ", " g ", " f ", " e ", " d ", " c ", " b ", " a "};
     private static Random rand = new Random();
 
 
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-
         out.print(ERASE_SCREEN);
 
-        drawHeaders(out);
+        drawWhiteBoard(out);
+        drawBlackBoard(out);
+    }
 
-        drawTicTacToeBoard(out);
+    private static void drawWhiteBoard(PrintStream out){
+        drawHeaders(out, whiteHeaders);
 
-        drawHeaders(out);
+        drawTicTacToeBoard(out, "white");
+
+        drawHeaders(out, whiteHeaders);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void drawHeaders(PrintStream out) {
+    private static void drawBlackBoard(PrintStream out){
+        drawHeaders(out, blackHeaders);
+
+        drawTicTacToeBoard(out, "black");
+
+        drawHeaders(out, blackHeaders);
+
+        out.print(SET_BG_COLOR_BLACK);
+        out.print(SET_TEXT_COLOR_WHITE);
+    }
+
+    private static void drawHeaders(PrintStream out, String[] headers) {
 
         setBlack(out);
-
-        String[] headers = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
+        out.print(EMPTY);
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
             drawHeader(out, headers[boardCol]);
         }
@@ -55,14 +71,33 @@ public class ChessBoard {
         setBlack(out);
     }
 // pass in a 2D array here and have it cycle throw each "row"
-    private static void drawTicTacToeBoard(PrintStream out) {
-
+    private static void drawTicTacToeBoard(PrintStream out, String team) {
+        int rowId;
+        if(team == "black"){
+            rowId = 1;
+        }
+        else{
+            rowId = 8;
+        }
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(SET_TEXT_COLOR_GREEN);
+            out.print(" " + rowId + " ");
             if(boardRow % 2 == 1) {
                 drawRowOfSquaresBlue(out);
             }
             else {
                 drawRowOfSquaresWhite(out);
+            }
+            out.print(SET_BG_COLOR_BLACK);
+            out.print(SET_TEXT_COLOR_GREEN);
+            out.print(" " + rowId + " ");
+            out.println();
+            if(team == "black"){
+                ++rowId;
+            }
+            else{
+                --rowId;
             }
 
         }
@@ -78,12 +113,11 @@ public class ChessBoard {
                 else{
                     setBlue(out);
                 }
-                printPlayer(out, rand.nextBoolean() ? X : O);
+                printPlayer(out, EMPTY);
 
                 setBlack(out);
             }
 
-            out.println();
         }
 
     private static void drawRowOfSquaresWhite(PrintStream out) {
@@ -95,12 +129,10 @@ public class ChessBoard {
             else{
                 setBlue(out);
             }
-            printPlayer(out, rand.nextBoolean() ? X : O);
+            printPlayer(out, EMPTY);
 
             setBlack(out);
         }
-
-        out.println();
     }
 
 
