5b3fcbb8d856438c2e8886cfee272600c1b5abb7
njseneca02 <njseneca@byu.edu>
2024-06-04 10:43:13 -0600

    added checkered coloring

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -9,8 +9,6 @@ import static ui.EscapeSequences.*;
 public class ChessBoard {
 
     private static final int BOARD_SIZE_IN_SQUARES = 8;
-    private static final int SQUARE_SIZE_IN_CHARS = 1;
-    private static final int LINE_WIDTH_IN_CHARS = 1;
     private static final String EMPTY = "   ";
     private static final String X = " X ";
     private static final String O = " O ";
@@ -39,22 +37,13 @@ public class ChessBoard {
         String[] headers = { " a ", " b ", " c ", " d ", " e ", " f ", " g ", " h " };
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
             drawHeader(out, headers[boardCol]);
-
-//            if (boardCol < BOARD_SIZE_IN_SQUARES - 1) {
-//                out.print(EMPTY.repeat(LINE_WIDTH_IN_CHARS));
-//            }
         }
 
         out.println();
     }
 
     private static void drawHeader(PrintStream out, String headerText) {
-        int prefixLength = SQUARE_SIZE_IN_CHARS / 2;
-        int suffixLength = SQUARE_SIZE_IN_CHARS - prefixLength - 1;
-
-        out.print(EMPTY.repeat(prefixLength));
         printHeaderText(out, headerText);
-        out.print(EMPTY.repeat(suffixLength));
     }
 
     private static void printHeaderText(PrintStream out, String player) {
@@ -69,18 +58,26 @@ public class ChessBoard {
     private static void drawTicTacToeBoard(PrintStream out) {
 
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
-
-            drawRowOfSquares(out);
+            if(boardRow % 2 == 1) {
+                drawRowOfSquaresBlue(out);
+            }
+            else {
+                drawRowOfSquaresWhite(out);
+            }
 
         }
     }
 // pass each row into here and it can check to see what piece it should print on the row
     // also need to add code so that before each row there is a number printed as well as after.
-    private static void drawRowOfSquares(PrintStream out) {
+    private static void drawRowOfSquaresBlue(PrintStream out) {
 
             for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
-                setWhite(out);
-
+                if(boardCol % 2 == 1) {
+                    setWhite(out);
+                }
+                else{
+                    setBlue(out);
+                }
                 printPlayer(out, rand.nextBoolean() ? X : O);
 
                 setBlack(out);
@@ -89,28 +86,27 @@ public class ChessBoard {
             out.println();
         }
 
-    private static void drawVerticalLine(PrintStream out) {
+    private static void drawRowOfSquaresWhite(PrintStream out) {
 
-        int boardSizeInSpaces = BOARD_SIZE_IN_SQUARES * SQUARE_SIZE_IN_CHARS +
-                (BOARD_SIZE_IN_SQUARES - 1) * LINE_WIDTH_IN_CHARS;
-
-        for (int lineRow = 0; lineRow < LINE_WIDTH_IN_CHARS; ++lineRow) {
-            setRed(out);
-            out.print(EMPTY.repeat(boardSizeInSpaces));
+        for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES; ++boardCol) {
+            if(boardCol % 2 == 0) {
+                setWhite(out);
+            }
+            else{
+                setBlue(out);
+            }
+            printPlayer(out, rand.nextBoolean() ? X : O);
 
             setBlack(out);
-            out.println();
         }
+
+        out.println();
     }
+
 
     private static void setWhite(PrintStream out) {
         out.print(SET_BG_COLOR_WHITE);
         out.print(SET_TEXT_COLOR_WHITE);
-    }
-
-    private static void setRed(PrintStream out) {
-        out.print(SET_BG_COLOR_RED);
-        out.print(SET_TEXT_COLOR_RED);
     }
 
     private static void setBlack(PrintStream out) {
@@ -118,8 +114,12 @@ public class ChessBoard {
         out.print(SET_TEXT_COLOR_BLACK);
     }
 
+    private static void setBlue(PrintStream out){
+        out.print(SET_BG_COLOR_BLUE);
+        out.print(SET_TEXT_COLOR_BLUE);
+    }
+
     private static void printPlayer(PrintStream out, String player) {
-        out.print(SET_BG_COLOR_WHITE);
         out.print(SET_TEXT_COLOR_BLACK);
 
         out.print(player);
